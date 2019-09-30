6c83f56e9bfabf8ff0588bf5c2d8802c41c0c993
ESA SPICE Service <esa_spice@sciops.esa.int>
2019-09-30 11:03:56 +0200

    Unity Training outcome

--- Assets/Spice/HelloWorld.cs
+++ Assets/Spice/HelloWorld.cs
@@ -4,45 +4,69 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 
-
 public class HelloWorld : MonoBehaviour
 {
 
     public string myName;
 
-    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)] private static extern void fibonacci_init(int a, int b);
-    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)]  private static extern bool fibonacci_next();
-    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)]  private static extern int fibonacci_index();
-    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)]  private static extern long fibonacci_current();
-    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)]  private static extern double GetPosition();
+    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)]  private static extern void LoadMK();
+    [DllImport("CSpiceLib", CharSet = CharSet.Ansi)]  private static extern float GetPosition(float et, int pos);
+
+    private float et0 = 599572869.184f;
+    private float et;
+    private float waitTime = 2.0f;
+    private float visualTime = 0.0f;
+    private char utc;
+
+    private Vector3 bodyPosition;
+    private float xPosition, yPosition, zPosition;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(myName + " I am alive!");
         Debug.Log("This is a C++ Function from the Dynamic Library");
+        LoadMK();
+        et = et0;
 
-        // Initialize a Fibonacci relation sequence.
-        fibonacci_init(1, 1);
-        // Write out the sequence values until overflow.
-        Debug.Log(GetPosition());
     }
+
 
     // Update is called once per frame
     void Update()
     {
 
- //       {
-//            do
-//            {
-//                Debug.Log(fibonacci_index() + ": "
-//                    + fibonacci_current());
-//            } while (fibonacci_next());
-//            // Report count of values written before overflow.
-//            Debug.Log(fibonacci_index() + 1 +
-//                " Fibonacci sequence values fit in an " +
-//                "unsigned 64-bit integer.");
-//        }
+        et += Time.fixedDeltaTime*2000;
+        visualTime += Time.fixedDeltaTime;
+
+        xPosition = GetPosition(et, 1);
+        yPosition = GetPosition(et, 2);
+        zPosition = GetPosition(et, 3);
+
+        // Move sphere around the circle.
+        bodyPosition = new Vector3(xPosition, yPosition, zPosition);
+        transform.position = bodyPosition;
+
+
+
+        // Check if we have reached beyond 2 seconds.
+        // Subtracting two is more accurate over time than resetting to zero.
+        if (visualTime > waitTime)
+        {
+           // Debug.Log(et);
+            Debug.Log(xPosition);
+            //Debug.Log(ycoord);
+            //Debug.Log(zcoord);
+            // Remove the recorded 2 seconds.
+            visualTime = 0.0f;
+
+        }
     }
+
+
+    
+
+
 }
 
